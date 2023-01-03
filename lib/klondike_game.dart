13d18e292fc51363584535e7422b273325c136e6
frankli <357019317@qq.com>
2023-01-03 17:08:27 +0800

    step 3

--- lib/klondike_game.dart
+++ lib/klondike_game.dart
@@ -1,8 +1,15 @@
+import 'dart:math';
+
+import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
-import 'package:flame/sprite.dart';
+import 'package:flutter_game_flame/components/foundation.dart';
 import 'package:flutter_game_flame/components/stock.dart';
 import 'package:flutter_game_flame/components/waste.dart';
+
+import 'components/card.dart';
+import 'components/pile.dart';
 
 class KlondikeGame extends FlameGame {
   static const double cardWidth = 1000.0;
@@ -20,13 +27,53 @@ class KlondikeGame extends FlameGame {
     final waste = Waste()
       ..size = cardSize
       ..position = Vector2(cardWidth + 2 * cardGap, cardGap);
-  }
-
-  Sprite klondikeSprite(double x, double y, double width, double height) {
-    return Sprite(
-      Flame.images.fromCache('klondike-sprites.png'),
-      srcPosition: Vector2(x, y),
-      srcSize: Vector2(width, height),
+    final foundations = List.generate(
+      4,
+      (index) => Foundation()
+        ..size = cardSize
+        ..position =
+            Vector2((index + 3) * (cardWidth + cardGap) + cardGap, cardGap),
     );
+    final piles = List.generate(
+        7,
+        (index) => Pile()
+          ..size = cardSize
+          ..position = Vector2(cardGap + index * (cardWidth + cardGap),
+              cardHeight + 2 * cardGap));
+
+    final world = World()
+      ..add(stock)
+      ..add(waste)
+      ..addAll(foundations)
+      ..addAll(piles);
+    add(world);
+
+    final camera = CameraComponent(world: world)
+      ..viewfinder.visibleGameSize =
+          Vector2(cardWidth * 7 + cardGap * 8, 4 * cardHeight + 3 * cardGap)
+      ..viewfinder.position = Vector2(cardWidth * 3.5 + cardGap * 4, 0)
+      ..viewfinder.anchor = Anchor.topCenter;
+    add(camera);
+
+    final random = Random();
+    for (var i = 0; i < 7; i++) {
+      for (var j = 0; j < 4; j++) {
+        final card = Card(random.nextInt(13) + 1, random.nextInt(4))
+          ..position = Vector2(100 + i * 1150, 100 + j * 1500)
+          ..addToParent(world);
+        if (random.nextDouble() < 0.9) {
+          // flip face up with 90% probability
+          card.flip();
+        }
+      }
+    }
   }
+}
+
+Sprite klondikeSprite(double x, double y, double width, double height) {
+  return Sprite(
+    Flame.images.fromCache('klondike-sprites.png'),
+    srcPosition: Vector2(x, y),
+    srcSize: Vector2(width, height),
+  );
 }
